cecda7ad4b406b5efdc4832333d578e89ed75409
samrpat <162509333+samrpat@users.noreply.github.com>
2026-08-31 16:10:25 -0400

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -10,6 +10,6 @@ export const firebaseConfig = {
   projectId: "big7rankings-aeb0c",
   storageBucket: "big7rankings-aeb0c.firebasestorage.app",
   messagingSenderId: "1069198918587",
-  appId: "1:1069198918587:web:47f18f627881c3ae82fa9e",
-  measurementId: "G-WDXVYMET60"
+  appId: "1:1069198918587:web:086619ca52e24f7882fa9e",
+  measurementId: "G-QLT4CFPPCW"
 };
